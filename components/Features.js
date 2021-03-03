fd866c417dfb4b14110c60706956777c21ce92a2
Gabriel Kling <gabrielkling@outlook.com>
2021-03-03 14:47:02 -0300

    Alteração de Texto

--- components/Features.js
+++ components/Features.js
@@ -13,8 +13,8 @@ const Features = () => {
                             <div className="service-one__single text-center">
                                 <div className="service-one__inner">
                                     <i className="service-one__icon dimon-icon-laptop"></i>
-                                    <h3><a href="#">Desenvolvimento <br /> Ágil</a></h3>
-                                    <p>Lorem ipsum is are <br /> many variations of <br /> pass of majority.</p>
+                                    <h3><a href="#">Criação <br /> de Websites</a></h3>
+                                    <p>Invista em um <br />site Profissional para a <br />sua empresa e Transforme <br />seu Digital.</p>
                                     <a href="#" className="service-one__link"><i className="dimon-icon-right-arrow"></i></a>
                                 </div>
                             </div>
@@ -24,7 +24,7 @@ const Features = () => {
                                 <div className="service-one__inner">
                                     <i className="service-one__icon dimon-icon-presentation"></i>
                                     <h3><a href="#">Marketing <br /> Digital</a></h3>
-                                    <p>Lorem ipsum is are <br /> many variations of <br /> pass of majority.</p>
+                                    <p>Planejamento e criação <br /> de estratégias de Marketing e <br /> Vendas para a sua empresa.</p>
                                     <a href="#" className="service-one__link"><i className="dimon-icon-right-arrow"></i></a>
                                 </div>
                             </div>
@@ -34,7 +34,7 @@ const Features = () => {
                                 <div className="service-one__inner">
                                     <i className="service-one__icon dimon-icon-target"></i>
                                     <h3><a href="#">Gerenciamento de <br /> Tráfego</a></h3>
-                                    <p>Lorem ipsum is are <br /> many variations of <br /> pass of majority.</p>
+                                    <p>Criação e gerenciamento de campanhas<br /> pagas em todas as <br /> plataformas de anúncio.</p>
                                     <a href="#" className="service-one__link"><i className="dimon-icon-right-arrow"></i></a>
                                 </div>
                             </div>
@@ -44,7 +44,7 @@ const Features = () => {
                                 <div className="service-one__inner">
                                     <i className="service-one__icon dimon-icon-visualization"></i>
                                     <h3><a href="#">Marketing de <br /> Conteúdo</a></h3>
-                                    <p>Lorem ipsum is are <br /> many variations of <br /> pass of majority.</p>
+                                    <p>Produção e criação de<br /> materiais estratégicamente <br /> elaborados para o seu público alvo.</p>
                                     <a href="#" className="service-one__link"><i className="dimon-icon-right-arrow"></i></a>
                                 </div>
                             </div>
